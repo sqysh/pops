043bcbb45e7cc55b817d7889f27221ce484bce87
Gregory Row <rowgregory@gmail.com>
2026-09-15 11:17:05 -0400

    chore:and add force dynamic to VenuesClient

--- app/(public)/venues/page.tsx
+++ app/(public)/venues/page.tsx
@@ -1,6 +1,8 @@
 import { getVenues } from '@/app/lib/actions/venue/getVenues'
 import { VenuesClient } from './VenuesClient'
 
+export const dynamic = 'force-dynamic'
+
 export default async function VenuesLayout() {
   const venues = await getVenues()
   return <VenuesClient venues={venues} />
